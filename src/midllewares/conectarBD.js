986c435a21b109197d730e6542d6bc86786b4ab8
Jônatas Henrique <jonatshenrique@gmail.com>
2023-08-14 08:36:13 -0300

    Corrigindo erro no banco

--- src/midllewares/conectarBD.js
+++ src/midllewares/conectarBD.js
@@ -3,7 +3,7 @@ const tratarErrosInesperados = require('../function/tratrarErrosInesperados');
 
 async function conectBancodeDados(req = null, res = null, next = null){
     try {
-        await mongoose.connect(process.env.MONGOBD_URI, { useNewUrlParser: true, useUnifiedTopology: true } )
+        await mongoose.connect(process.env.MONGOBD_URI, {  useNewUrlParser: true, useUnifiedTopology: true } )
         console.log('Conectado ao banco de dados')
         try { next(); } catch { };
         return mongoose
